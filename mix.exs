40fdeb5c1f514c7e49b69033daf194e0a50ea8b5
Grey Baker <greysteil@gmail.com>
2018-03-11 00:49:03 +0000

    Initial mixfile

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule DependabotTest.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :dependabot_test,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.3"},
+      {:money, "~> 1.2.1"},
+      {:stripity_stripe, github: "code-corps/stripity_stripe", ref: "v2.0.0-alpha.1"}
+    ]
+  end
+end
